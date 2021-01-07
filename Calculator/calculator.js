8cb728cd446cfd9756e40ead282bda25ef77448d
Michael Matich <matich.mike@gmail.com>
2021-01-07 11:41:43 -0500

    add calc working

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
-  res.send('Hello');
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+  const num1 = req.body.num1;
+  const num2 = req.body.num2;
+  res.send(`the answer is ${(Number(num1) + Number(num2))}`);
 });
 
 app.listen(3000, function() {
