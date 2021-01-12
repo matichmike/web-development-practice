1d0241c90d3316eebbf833226db372205512e67e
Michael Matich <matich.mike@gmail.com>
2021-01-12 09:46:26 -0500

    signup page added

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -4,6 +4,21 @@ const https = require("https");
 
 const app = express();
 
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+})
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/", function(req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  console.log(firstName, lastName, email)
+})
+
 app.listen(3000, function() {
   console.log("server is running on 3000");
 });
