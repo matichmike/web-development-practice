84a04e80fbdbc8c9cba3262b0da95699b8e82e86
Michael Matich <matich.mike@gmail.com>
2021-01-13 10:41:13 -0500

    mailchimp post works

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -16,9 +16,48 @@ app.post("/", function(req, res) {
   const lastName = req.body.lName;
   const email = req.body.email;
 
-  console.log(firstName, lastName, email)
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  }
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us7.api.mailchimp.com/3.0/lists/b2b16f33b8";
+  const options = {
+    method: "POST",
+    auth: "mike1:9145befd379831fa06cab1b74dd2f72f-us7"
+  };
+  const request = https.request(url, options, function(response) {
+    if(response.statusCode === 200) {
+      res.sendFile(`${__dirname}/success.html`);
+    } else {
+      res.sendFile(`${__dirname}/failure.html`);
+    };
+    response.on("data", function(data) {
+      console.log(JSON.parse(data))
+    })
+  })
+
+  request.write(jsonData);
+  request.end();
 })
+
+app.post("/failure" , function(req, res) {
+  res.redirect("/");
+});
 
 app.listen(3000, function() {
   console.log("server is running on 3000");
 });
+
+// 9145befd379831fa06cab1b74dd2f72f-us7
+// b2b16f33b8
